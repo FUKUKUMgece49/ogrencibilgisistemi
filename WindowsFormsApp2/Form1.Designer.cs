c95d3aca9760b21a79a3c1b7daffc3492f345fdc
Melike Korkmaz <156064520+FUKUKUMgece49@users.noreply.github.com>
2024-08-26 20:39:02 +0300

    staj projeleri-2
    c# ile yapılmış öğrenci bilgi sistemi

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace OgrenciBilgiSistemi
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxBirthDate = new System.Windows.Forms.TextBox();
+            this.textBoxClass = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonList = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.labelBirthDate = new System.Windows.Forms.Label();
+            this.labelClass = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 192);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(1035, 347);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(112, 15);
+            this.textBoxID.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.ReadOnly = true;
+            this.textBoxID.Size = new System.Drawing.Size(132, 22);
+            this.textBoxID.TabIndex = 1;
+            this.textBoxID.TextChanged += new System.EventHandler(this.textBoxID_TextChanged);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(112, 47);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(132, 22);
+            this.textBoxName.TabIndex = 2;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(112, 79);
+            this.textBoxLastName.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(132, 22);
+            this.textBoxLastName.TabIndex = 3;
+            this.textBoxLastName.TextChanged += new System.EventHandler(this.textBoxLastName_TextChanged);
+            // 
+            // textBoxBirthDate
+            // 
+            this.textBoxBirthDate.Location = new System.Drawing.Point(112, 111);
+            this.textBoxBirthDate.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxBirthDate.Name = "textBoxBirthDate";
+            this.textBoxBirthDate.Size = new System.Drawing.Size(132, 22);
+            this.textBoxBirthDate.TabIndex = 4;
+            this.textBoxBirthDate.TextChanged += new System.EventHandler(this.textBoxBirthDate_TextChanged);
+            // 
+            // textBoxClass
+            // 
+            this.textBoxClass.Location = new System.Drawing.Point(112, 143);
+            this.textBoxClass.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxClass.Name = "textBoxClass";
+            this.textBoxClass.Size = new System.Drawing.Size(132, 22);
+            this.textBoxClass.TabIndex = 5;
+            this.textBoxClass.TextChanged += new System.EventHandler(this.textBoxClass_TextChanged);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(253, 15);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(100, 28);
+            this.buttonAdd.TabIndex = 6;
+            this.buttonAdd.Text = "EKLE";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(253, 47);
+            this.buttonUpdate.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(100, 28);
+            this.buttonUpdate.TabIndex = 7;
+            this.buttonUpdate.Text = "GÜNCELLE";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(253, 79);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(100, 28);
+            this.buttonDelete.TabIndex = 8;
+            this.buttonDelete.Text = "SİL";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(253, 111);
+            this.buttonSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(100, 28);
+            this.buttonSearch.TabIndex = 9;
+            this.buttonSearch.Text = "ARA";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // buttonList
+            // 
+            this.buttonList.Location = new System.Drawing.Point(253, 143);
+            this.buttonList.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonList.Name = "buttonList";
+            this.buttonList.Size = new System.Drawing.Size(100, 28);
+            this.buttonList.TabIndex = 10;
+            this.buttonList.Text = "LİSTELE";
+            this.buttonList.UseVisualStyleBackColor = true;
+            this.buttonList.Click += new System.EventHandler(this.buttonList_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(361, 15);
+            this.buttonClear.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(100, 28);
+            this.buttonClear.TabIndex = 11;
+            this.buttonClear.Text = "KAYDI SİL";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(16, 18);
+            this.labelID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(70, 16);
+            this.labelID.TabIndex = 12;
+            this.labelID.Text = "Öğrenci ID";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(16, 50);
+            this.labelName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(58, 16);
+            this.labelName.TabIndex = 13;
+            this.labelName.Text = "İsim / Ad";
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(16, 82);
+            this.labelLastName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(50, 16);
+            this.labelLastName.TabIndex = 14;
+            this.labelLastName.Text = "Soyadı";
+            // 
+            // labelBirthDate
+            // 
+            this.labelBirthDate.AutoSize = true;
+            this.labelBirthDate.Location = new System.Drawing.Point(16, 114);
+            this.labelBirthDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelBirthDate.Name = "labelBirthDate";
+            this.labelBirthDate.Size = new System.Drawing.Size(88, 16);
+            this.labelBirthDate.TabIndex = 15;
+            this.labelBirthDate.Text = "Doğum Tarihi";
+            // 
+            // labelClass
+            // 
+            this.labelClass.AutoSize = true;
+            this.labelClass.Location = new System.Drawing.Point(16, 146);
+            this.labelClass.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelClass.Name = "labelClass";
+            this.labelClass.Size = new System.Drawing.Size(32, 16);
+            this.labelClass.TabIndex = 16;
+            this.labelClass.Text = "Sınıf";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.labelClass);
+            this.Controls.Add(this.labelBirthDate);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonList);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxClass);
+            this.Controls.Add(this.textBoxBirthDate);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "ÖĞRENCİ BİLGİ SİSTEMİ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxBirthDate;
+        private System.Windows.Forms.TextBox textBoxClass;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonList;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.Label labelBirthDate;
+        private System.Windows.Forms.Label labelClass;
+    }
+}
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace OgrenciBilgiSistemi
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=DESKTOP-7EPCTQ4;Initial Catalog=OgrenciBilgiSistemi;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO Ogrenciler (Name, LastName, BirthDate, Class) VALUES (@Name, @LastName, @BirthDate, @Class)", con);
+                    cmd.Parameters.AddWithValue("@Name", textBoxName.Text);
+                    cmd.Parameters.AddWithValue("@LastName", textBoxLastName.Text);
+                    cmd.Parameters.AddWithValue("@BirthDate", string.IsNullOrEmpty(textBoxBirthDate.Text) ? (object)DBNull.Value : textBoxBirthDate.Text);
+                    cmd.Parameters.AddWithValue("@Class", textBoxClass.Text);
+                    cmd.ExecuteNonQuery();
+                }
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("UPDATE Ogrenciler SET Name=@Name, LastName=@LastName, BirthDate=@BirthDate, Class=@Class WHERE ID=@ID", con);
+                    cmd.Parameters.AddWithValue("@ID", textBoxID.Text);
+                    cmd.Parameters.AddWithValue("@Name", textBoxName.Text);
+                    cmd.Parameters.AddWithValue("@LastName", textBoxLastName.Text);
+                    cmd.Parameters.AddWithValue("@BirthDate", string.IsNullOrEmpty(textBoxBirthDate.Text) ? (object)DBNull.Value : textBoxBirthDate.Text);
+                    cmd.Parameters.AddWithValue("@Class", textBoxClass.Text);
+                    cmd.ExecuteNonQuery();
+                }
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("DELETE FROM Ogrenciler WHERE ID=@ID", con);
+                    cmd.Parameters.AddWithValue("@ID", textBoxID.Text);
+                    cmd.ExecuteNonQuery();
+                }
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Ogrenciler WHERE Name LIKE '%' + @Search + '%'", con);
+                    da.SelectCommand.Parameters.AddWithValue("@Search", textBoxName.Text);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void buttonList_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxID.Clear();
+            textBoxName.Clear();
+            textBoxLastName.Clear();
+            textBoxBirthDate.Clear();
+            textBoxClass.Clear();
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                textBoxID.Text = row.Cells["ID"].Value.ToString();
+                textBoxName.Text = row.Cells["Name"].Value.ToString();
+                textBoxLastName.Text = row.Cells["LastName"].Value.ToString();
+                textBoxBirthDate.Text = row.Cells["BirthDate"].Value.ToString();
+                textBoxClass.Text = row.Cells["Class"].Value.ToString();
+            }
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Ogrenciler", con);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void textBoxID_TextChanged(object sender, EventArgs e)
+        {
+            // ID için işlemler
+            if (string.IsNullOrEmpty(textBoxID.Text) || (int.TryParse(textBoxID.Text, out int id) && id == 0))
+            {
+                // ID boşsa veya sıfırsa yapılacak işlemler
+            }
+        }
+
+        private void textBoxBirthDate_TextChanged(object sender, EventArgs e)
+        {
+            // Doğum tarihi için işlemler
+            if (string.IsNullOrEmpty(textBoxBirthDate.Text))
+            {
+                // Doğum tarihi boşsa yapılacak işlemler
+            }
+            else if (DateTime.TryParse(textBoxBirthDate.Text, out DateTime birthDate))
+            {
+                // Geçerli bir tarih formatıysa yapılacak işlemler
+            }
+            else
+            {
+                // Geçersiz tarih formatı
+            }
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+            // İsim için işlemler
+        }
+
+        private void textBoxLastName_TextChanged(object sender, EventArgs e)
+        {
+            // Soyisim için işlemler
+        }
+
+        private void textBoxClass_TextChanged(object sender, EventArgs e)
+        {
+            // Sınıf için işlemler
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
